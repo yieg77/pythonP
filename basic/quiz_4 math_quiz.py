11cd975a190f43ac6c72e7e95a471074d86f3391
YEG <yieg77@naver.com>
2019-09-04 12:58:45 +0900

    0904 exam

---
+++ basic/quiz_4 math_quiz.py
@@ -0,0 +1,30 @@
+import random
+
+cnt_o = 0
+a=['+','-','*','/']
+#print(random.random())
+#print(random.randint(1, 10))
+#print(random.choice(a))  # 중복허용
+#print(random.sample)  # 중복X
+#random.shuffle(a)
+#print(a)
+
+for i in range(1,6):
+    quiz = str(random.randint(1,100)) + random.choice(a) +str(random.randint(1,100))
+    print(quiz+' = ', end='')
+    ans = int(eval(quiz))
+
+    print(ans)
+    user_ans = input()
+
+    if str(ans) == user_ans:
+        print('정답')
+        cnt_o += 1
+    else:
+        print('오답')
+
+print(str(cnt_o)+'개 맞음')
+
+
+
+
